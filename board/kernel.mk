4ce54268eccbbc083410131e6c5f54513e22c090
ishantvivek <ishantvivek1311@gmail.com>
2017-08-29 12:14:16 +0000

    tomato: Compress ramdisk for kernel also
    * Since we switched to inbuilt module , boot.img size exceeeds , so compress ramdisk to make things fit

--- board/kernel.mk
+++ board/kernel.mk
@@ -2,6 +2,6 @@
 BOARD_DTBTOOL_ARGS := -2
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_SEPARATED_DT := true
-LZMA_RAMDISK_TARGETS := recovery
+LZMA_RAMDISK_TARGETS := boot,recovery
 TARGET_KERNEL_CONFIG := lineageos_tomato_defconfig
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
